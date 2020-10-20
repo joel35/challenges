d46f119e477096dc6011c3b5c2ac22d0526ed1f9
joel <system@icenilabs.com>
2020-10-20 10:29:45 +0100

    21 done

--- 21/cars.py
+++ 21/cars.py
@@ -10,22 +10,33 @@ cars = {
 def get_all_jeeps(cars=cars):
     """return a comma  + space (', ') separated string of jeep models
        (original order)"""
-    pass
+    return ', '.join(cars['Jeep'])
 
 
 def get_first_model_each_manufacturer(cars=cars):
     """return a list of matching models (original ordering)"""
-    pass
+    return [cars[m][0] for m in cars]
 
 
 def get_all_matching_models(cars=cars, grep='trail'):
     """return a list of all models containing the case insensitive
        'grep' string which defaults to 'trail' for this exercise,
        sort the resulting sequence alphabetically"""
-    pass
+    matching = []
+    for manufacturer in cars.keys():
+        for model in cars[manufacturer]:
+            matching.append(model) if grep.lower() in model.lower() else ''
+    return sorted(matching)
 
 
 def sort_car_models(cars=cars):
     """return a copy of the cars dict with the car models (values)
        sorted alphabetically"""
-    pass
+    return {k: sorted(v) for (k, v) in cars.items()}
+
+
+
+# print(get_all_jeeps())
+# print(get_first_model_each_manufacturer())
+print(get_all_matching_models())
+# print(sort_car_models())
