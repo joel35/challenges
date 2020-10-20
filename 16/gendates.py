a5575669946990ba4260ab53827ce3e095f4250e
joel <system@icenilabs.com>
2020-10-20 09:37:02 +0100

    16

--- 16/gendates.py
+++ 16/gendates.py
@@ -10,12 +10,9 @@ def gen_special_pybites_dates():
     while True:
         birthday = date.replace(year=date.year + count)
         hundred_days = datetime(date.year, date.month, date.day) + timedelta(days=count2 * 100)
-
         if birthday <= hundred_days:
             count += 1
             yield birthday
         else:
             count2 += 1
             yield hundred_days
-
-
