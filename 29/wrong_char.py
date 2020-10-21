54c0fcc403f1eec1e77ac2801d2c11f11ee2d2ab
joel <system@icenilabs.com>
2020-10-21 11:49:06 +0100

    29 done

--- 29/wrong_char.py
+++ 29/wrong_char.py
@@ -1,10 +1,11 @@
 def get_index_different_char(chars):
     alphanumeric = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
 
-    alpha = [char for char in chars if str(char) in alphanumeric and char is not '']
-    not_alpha = [char for char in chars if str(char) not in alphanumeric or char is '']
+    alpha = [char for char in chars if str(char) in alphanumeric and char != '']
+    not_alpha = [char for char in chars if str(char) not in alphanumeric or char == '']
     diff_item = not_alpha if len(alpha) > len(not_alpha) else alpha
 
     for index, item in enumerate(chars):
         if item in diff_item:
             return index
+
