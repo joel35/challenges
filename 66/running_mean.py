6aad1faedfe45195abf8ff13caf4011b7d1e9e06
joel <system@icenilabs.com>
2020-11-11 17:09:28 +0000

    66 added

---
+++ 66/running_mean.py
@@ -0,0 +1,11 @@
+def running_mean(sequence):
+    """Calculate the running mean of the sequence passed in,
+       returns a sequence of same length with the averages.
+       You can assume all items in sequence are numeric."""
+
+    pass
+
+
+
+if __name__ == '__main__':
+    print(running_mean([1, 2, 3]))
