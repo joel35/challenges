a500222f9f3f75e430d416bcc3be767e4303422f
joel <system@icenilabs.com>
2020-11-12 10:01:03 +0000

    68 done

---
+++ 68/clean.py
@@ -0,0 +1,5 @@
+import string
+
+def remove_punctuation(input_string):
+    """Return a str with punctuation chars stripped out"""
+    return ''.join([char for char in input_string if char not in list(string.punctuation)])
