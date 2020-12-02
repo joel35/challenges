79f134d6972caf06bec430f1320ec797e22cf6e4
joel <system@icenilabs.com>
2020-12-02 10:46:48 +0000

    180

---
+++ 180/names.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+# fake data from https://www.mockaroo.com
+data = """last_name,first_name,country_code
+Watsham,Husain,ID
+Harrold,Alphonso,BR
+Apdell,Margo,CN
+Tomblings,Deerdre,RU
+Wasielewski,Sula,ID
+Jeffry,Rudolph,TD
+Brenston,Luke,SE
+Parrett,Ines,CN
+Braunle,Kermit,PL
+Halbard,Davie,CN"""
+
+
+def group_names_by_country(data: str = data) -> defaultdict:
+    countries = defaultdict(list)
+    items = [x.split(',') for x in data.splitlines()[1:]]
+
+    for last_name, first_name, country_code in items:
+        countries[country_code].append(f'{first_name} {last_name}')
+
+    return countries
+
