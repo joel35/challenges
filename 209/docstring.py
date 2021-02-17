3a3aba838116a08289dbc2540d0d289f5410c1aa
joel35 <system@icenilabs.com>
2021-02-17 09:43:35 +0000

    209 done

---
+++ 209/docstring.py
@@ -0,0 +1,13 @@
+def sum_numbers(numbers):
+    """
+    Sums numbers
+
+    :param numbers: a list of numbers
+    :type numbers: list
+
+    :raises TypeError: If not all numeric values are passed in.
+
+    :return: Sum of numbers
+    :rtype: int
+    """
+    pass
