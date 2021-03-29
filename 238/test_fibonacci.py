33703ee9f6b71d3b84e287f3980b0da75b2e3ea5
joel35 <system@icenilabs.com>
2021-03-29 09:42:52 +0100

    238 done

---
+++ 238/test_fibonacci.py
@@ -0,0 +1,17 @@
+import pytest
+from fibonacci import fib
+
+
+# write one or more pytest functions below, they need to start with test_
+
+def test_negative():
+    with pytest.raises(ValueError):
+        fib(-1)
+
+
+def test_fib_list():
+    fib_list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711,
+                28657, 46368, 75025, 121393, 196418, 317811]
+
+    for i, num in enumerate(fib_list):
+        assert fib(i) == num
