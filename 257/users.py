2841ff85965115c5a59cff92e0b11acb75d41f00
joel35 <system@icenilabs.com>
2021-03-31 10:58:28 +0100

    257 done

---
+++ 257/users.py
@@ -0,0 +1,17 @@
+import re
+
+
+def get_users(passwd: str) -> dict:
+    """Split password output by newline,
+      extract user and name (1st and 5th columns),
+      strip trailing commas from name,
+      replace multiple commas in name with a single space
+      return dict of keys = user, values = name.
+    """
+    lines = [line.split(':') for line in passwd.strip().splitlines()]
+
+    return {
+        user: re.sub(",+", ' ', name).strip() if name else 'unknown'
+        for (user, name)
+        in zip([line[0] for line in lines], [line[4] for line in lines])
+    }
