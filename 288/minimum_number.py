af0fd61c9c8c32c566c55a7b19ae84988aecd79f
joel35 <system@icenilabs.com>
2021-04-15 10:51:44 +0100

    288 done

--- 288/minimum_number.py
+++ 288/minimum_number.py
@@ -2,5 +2,4 @@ from typing import List
 
 
 def minimum_number(digits: List[int]) -> int:
-    deduped = set([d for d in digits if d])
-    return int(''.join(map(str, sorted(deduped)))) if deduped else 0
+    return int(''.join(map(str, sorted(set(digits))))) if digits else 0
