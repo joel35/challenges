a7c576eac4f6caf2d488626f78700fdd65c5a6a8
joel35 <system@icenilabs.com>
2021-09-15 10:15:07 +0100

    114 done

--- 114/color.py
+++ 114/color.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import urllib.request
-from PIL import ImageColor
 import re
 
 # PREWORK (don't modify): import colors, save to temp file and import
@@ -33,9 +32,9 @@ class Color:
         is_valid_hex = re.compile("^#[A-Fa-f0-9]{6}$")
 
         if not isinstance(hex_val, str) or not re.search(is_valid_hex, hex_val):
-            raise ValueError('Input must be a valid hex string')
+            raise ValueError('Input must be a valid six-character hex string')
 
-        return ImageColor.getrgb(hex_val)
+        return int(hex_val[1:3], 16), int(hex_val[3:5], 16), int(hex_val[5:7], 16)
 
     @staticmethod
     def rgb2hex(rgb):
