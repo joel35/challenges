a3bb1e174b3d68ed1b5df90cfa060070c85b1871
joel35 <system@icenilabs.com>
2021-09-20 09:25:41 +0100

    199 done

--- 199/person.py
+++ 199/person.py
@@ -1,18 +1,18 @@
 class Person:
-    def __repr__(self):
+    def __str__(self):
         return 'I am a person'
 
 
 class Mother(Person):
-    def __repr__(self):
-        return f'{super().__repr__()} and awesome mom'
+    def __str__(self):
+        return f'{super().__str__()} and awesome mom'
 
 
 class Father(Person):
-    def __repr__(self):
-        return f'{super().__repr__()} and cool daddy'
+    def __str__(self):
+        return f'{super().__str__()} and cool daddy'
 
 
 class Child(Father, Mother):
-    def __repr__(self):
+    def __str__(self):
         return 'I am the coolest kid'
